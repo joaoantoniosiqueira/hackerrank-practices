5846f772e1d8dba1ed5dfbaf07ebe89e7741e0d1
Joao Antonio Ferreira Siqueira <joao.calvet.siqueira@gmail.com>
2020-08-05 21:18:48 -0300

    Solucao do problema Java Regex

---
+++ src/main/java/br/com/amplitude/tests/hackerrank/JavaRegex.java
@@ -0,0 +1,22 @@
+package br.com.amplitude.tests.hackerrank;
+
+import java.util.Scanner;
+
+public class JavaRegex {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex {
+    String pattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+}
